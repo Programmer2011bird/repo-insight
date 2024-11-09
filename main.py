0b50e94498d651bc7547194f56c22a6949be0c0b
Programmer2011bird <maysamshaker7@gmail.com>
2024-11-09 09:43:00 +0330

    implementing repositories search ( NOTE: Not Complete, Does Not Return Insights YET )

--- main.py
+++ main.py
@@ -5,16 +5,35 @@ class API_HANDLER:
     def __init__(self) -> None:
         self.URL: str = "https://api.github.com/"
 
-    def searchRepo(self, searchQuery: str):
+    def searchRepo(self, searchQuery: str) -> dict[str, str]:
         self.ENDPOINT: str = f"{self.URL}/search/repositories?q={searchQuery}"
+        self.RESPONSE: requests.Response = requests.get(self.ENDPOINT)
+        self.JSON_RESPONSE: dict = dict(self.RESPONSE.json())
+        
+        self.USEFULL_INFO: dict[str, str] = {}
+        # TODO : set this up to return insights on the dict ( after implementing getInfo function)
+        for index, item in enumerate(self.JSON_RESPONSE["items"]):
+            self.USEFULL_INFO.update({str(index) : item["full_name"]})
 
-    def searchUser(self, searchQuery: str):
+        return self.USEFULL_INFO
+
+    def searchUser(self, searchQuery: str) -> None:
         self.ENDPOINT: str = f"{self.URL}/search/users?q={searchQuery}"
         
     def getInfo(self, owner: str, repoName: str):
         self.ENDPOINT: str = f"{self.URL}/repos/{owner}/{repoName}"
+        #"fullName" : "",
+        #"license" : "",
+        #"htmlUrl" : "",
+        #"cloneUrl" : "",
+        #"language" : "",
+        #"forkCounts" : "",
+        #"stars" : "",
+        #"watchers" : "",
+        #"description" : ""
 
 
 if __name__ == "__main__":
-    API_HANDLER()
+    API: API_HANDLER = API_HANDLER()
+    print(API.searchRepo("Neovim"))
 
